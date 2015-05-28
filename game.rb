2c799ed8ac73896218ab11de45c611694325e939
Geovanna RC <gechro@outlook.com>
2015-05-28 12:44:49 -0400

    Setup Game class with broad names for methods

---
+++ game.rb
@@ -0,0 +1,17 @@
+class Game
+  def initialize
+    # @board = Board.new # => {"CapitalNUMBER": PieceObject}
+  end
+
+  def which_piece
+    #gets users's move (which piece)
+  end
+
+  def available_positions
+    #gives list of all available positions for the piece
+  end
+
+  def what_move
+    #asks for move... which position from list of available positions
+  end
+end

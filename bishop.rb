8eb4e6bd73f0b2bdef18c50b45572e6f72c9f0c3
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-05-30 08:00:39 -0400

    updated bishop with initialize method that includes an instance variable of 'picture' that gives it the appropriate ascii art based on color

--- bishop.rb
+++ bishop.rb
@@ -1,6 +1,18 @@
 require_relative 'piece'
 
 class Bishop < Piece
+  attr_accessor :captured, :color
+  attr_reader :color, :picture
+  def initialize(color = "black")
+    @captured = false
+    @color = color
+    if self.color == "black"
+      @picture = "♝"
+    else
+      @picture = "♗"
+    end
+  end
+
   def how_can_i_move(xy)
     x = split_coordinate(xy)[0]
     y = split_coordinate(xy)[1]
@@ -23,3 +35,5 @@ class Bishop < Piece
     possible_array
   end
 end
+
+
